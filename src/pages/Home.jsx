5c45bf93152bab63851992960ec26d94fc7e2fe1
Theresia <hobeikatheresia5@gmail.com>
2026-07-06 15:06:57 +0300

    added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+
 import { getProducts } from "../DATA/products";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   const products = getProducts();
@@ -17,23 +18,7 @@ export default function Home() {
         <h2 className="page-title">Our Products</h2>
         <div className="product-grid">
           {products.map((product) => (
-            <div className="product-card" key={product.id}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-card-image"
-              />
-              <div className="product-card-content">
-                <h3 className="product-card-name">{product.name}</h3>
-                <p className="product-card-price">${product.price}</p>
-                <div className="product-card-actions">
-                  <Link to={`/products/${product.id}`} className="btn btn-secondary">
-                    View details
-                  </Link>
-                  <button className="btn btn-primary">Add to cart</button>
-                </div>
-              </div>
-            </div>
+            <ProductCard product={product} key={product.id}/>
           ))}
         </div>
       </div>
